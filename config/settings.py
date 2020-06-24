fba8f4a53f2126ccb79501dd240d1b813d3deb18
Fabiano <fabiomsrs@gmail.com>
2020-06-24 15:05:18 -0300

    creating views and adding tempaltes

--- config/settings.py
+++ config/settings.py
@@ -15,6 +15,8 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
+def base_dir_join(*args):
+    return os.path.join(BASE_DIR, *args)
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
@@ -55,7 +57,7 @@ ROOT_URLCONF = 'config.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        "DIRS": [base_dir_join("templates")],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- core/views.py
+++ core/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from django.shortcuts import get_object_or_404, render
+from django.urls import reverse
+from django.utils import timezone
+from django.views import generic
+
+from .models import Post, Category 
+
 
 # Create your views here.
+
+class IndexView(generic.ListView):
+    template_name = 'core/index.html'
+    # context_object_name = 'post_list'
+
+    def get_queryset(self):        
+        return Post.objects.all()
