8916f084ecffac02bb1b1e702798eb69a37793e8
Fabiano <fabiomsrs@gmail.com>
2020-06-24 21:07:18 -0300

    route '/' redirect to 'admin/'

--- config/urls.py
+++ config/urls.py
@@ -18,7 +18,7 @@ from django.urls import path, include
 from django.shortcuts import redirect
 
 urlpatterns = [
-    path("", lambda request : redirect("/home/")),    
+    path("", lambda request : redirect("/admin/")),    
     path('admin/', admin.site.urls),
     path("", include("core.urls"), name="core"),
     path("", include("api.urls"), name="api"),
